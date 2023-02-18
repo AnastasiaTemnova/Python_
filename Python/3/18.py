ccfbcb116355241a56b843e738e8252365aa87d0
AnastasiaTemnova <117480782+AnastasiaTemnova@users.noreply.github.com>
2023-02-18 21:52:44 +0400

    домашняя 3

---
+++ Python/3/18.py
@@ -0,0 +1,21 @@
+#Требуется найти в массиве A[1..N] самый близкий по величине элемент к заданному числу X. 
+# Пользователь в первой строке вводит натуральное число N – количество элементов в массиве. 
+# В последующих строках записаны N целых чисел Ai. 
+# Последняя строка содержит число X
+
+from random import randint
+size = int(input('Введите длину массива N: '))
+numbers = tuple(randint(1, size) for _ in range(size))
+print(*numbers)
+num_X = int(input('Введите число X: '))
+mod = 0
+flag = False
+for _ in range(len(set(numbers))):
+    for i in set(numbers):
+        if i == num_X - mod or i == num_X + mod:
+            print(i)
+            flag = True
+            break
+    if flag:
+        break
+    mod += 1
